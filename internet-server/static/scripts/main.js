7d8898b0b5cef214cf7cb005af30a69887b0f88b
⛧ <matthias.goffette@telecom-sudparis.eu>
2019-12-10 23:30:19 +0100

    solve #9 : refresh game data every second

---
+++ internet-server/static/scripts/main.js
@@ -0,0 +1,46 @@
+/**
+ * Script using JQuery
+ */
+
+
+/**
+ *
+ * @param cookies: int
+ */
+function update_cookies_nbr(cookies) {
+    $("#cookies_number").text(cookies);
+}
+
+
+/**
+ *
+ * @param upgrades
+ */
+function update_upgrades(upgrades) {
+    // upgrade stuff to buy #upgrades
+    // we'll assume no upgrades have been added
+    $(".upgrade-sold").each(function( index ) {
+        //console.log( index + ": " + $( this ).text() );
+        $(this).text(upgrades[index]["name"] + " : " + upgrades[index]["base_cost"] + " cookies");
+    });
+
+    $(".upgrade-bought").each(function( index ) {
+        //console.log(upgrades[index]["name"] + " : " + upgrades[index]["number"]);
+        $(this).text(upgrades[index]["name"] + " : " + upgrades[index]["number"]);
+    });
+    // upgrade stuff owned  #list
+}
+
+/**
+ * Get the suggested tracks
+ */
+function refresh_gamedata() {
+    $.get("/refresh", function (data) {
+        let cookies_nbr = data.cookies;
+        let upgrades = data.upgrades;
+        update_cookies_nbr(cookies_nbr);
+        update_upgrades(upgrades);
+    });
+}
+
+setInterval(refresh_gamedata, 1000);
